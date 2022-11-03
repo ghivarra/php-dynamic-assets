ada144dedcc92c4079386af2b774935d9718c9d8
WIN 10 Desktop GVR <gsenandika@gmail.com>
2022-11-03 20:18:26 +0700

    add home controller

---
+++ app/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php namespace App\Controller;
+
+/**
+ * Home Controller
+ *
+ * "I treat my works as my own child, be careful with my childrens"
+ *
+ * Created with love and proud by Ghivarra Senandika Rushdie
+ *
+ * @package PHP Dynamic Assets
+ *
+ * @var https://github.com/ghivarra
+ * @var https://facebook.com/bcvgr
+ * @var https://twitter.com/ghivarra
+ *
+**/
+
+class HomeController
+{
+	public function index()
+	{
+		d($_SERVER);
+	}
+
+	//=================================================================================================================
+}
