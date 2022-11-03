dd424cf658394289033f2475a6be9cb96d298f73
WIN 10 Desktop GVR <gsenandika@gmail.com>
2022-11-03 20:18:46 +0700

    add str_contains functions fallback

--- app/System/Helper.php
+++ app/System/Helper.php
@@ -1,1 +1,30 @@
-Helper
+<?php 
+
+/**
+ * Global Helper
+ *
+ * "I treat my works as my own child, be careful with my childrens"
+ *
+ * Created with love and proud by Ghivarra Senandika Rushdie
+ *
+ * @package Company Profile Website
+ *
+ * @var https://github.com/ghivarra
+ * @var https://facebook.com/bcvgr
+ * @var https://twitter.com/ghivarra
+ *
+**/
+
+/**
+ *
+ * Check String Exist
+ *
+ * @return bool
+ *
+**/
+if (!function_exists('str_contains'))
+{
+    function str_contains($haystack, $needle) {
+        return $needle !== '' && mb_strpos($haystack, $needle) !== FALSE;
+    }
+}
