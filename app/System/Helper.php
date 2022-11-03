6e00757efb7c0e4b0b319e5f2a98d767d44233b0
WIN 10 Desktop GVR <gsenandika@gmail.com>
2022-11-04 01:24:26 +0700

    add random string functions

--- app/System/Helper.php
+++ app/System/Helper.php
@@ -24,7 +24,8 @@
 **/
 if (!function_exists('str_contains'))
 {
-    function str_contains($haystack, $needle) {
+    function str_contains($haystack, $needle)
+    {
         return $needle !== '' && mb_strpos($haystack, $needle) !== FALSE;
     }
 }
@@ -38,8 +39,67 @@ if (!function_exists('str_contains'))
 **/
 if (!function_exists('dd'))
 {
-    function dd($data) {
+    function dd($data)
+    {
         d($data);
         exit;
+    }
+}
+
+/**
+* Create a Random String
+*
+* Useful for generating passwords or hashes.
+*
+* @param string $type Type of random string.  basic, alpha, alnum, numeric, nozero, md5, sha1, and crypto
+* @param int    $len  Number of characters
+*/
+
+if (! function_exists('random_string'))
+{
+    function random_string(string $type = 'alnum', int $len = 8): string
+    {
+        switch ($type) {
+            case 'alnum':
+            case 'numeric':
+            case 'nozero':
+            case 'alpha':
+                switch ($type) {
+                    case 'alpha':
+                        $pool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                        break;
+
+                    case 'alnum':
+                        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                        break;
+
+                    case 'numeric':
+                        $pool = '0123456789';
+                        break;
+
+                    case 'nozero':
+                        $pool = '123456789';
+                        break;
+                }
+
+                return substr(str_shuffle(str_repeat($pool, (int) ceil($len / strlen($pool)))), 0, $len);
+
+            case 'md5':
+                return md5(uniqid((string) mt_rand(), true));
+
+            case 'sha1':
+                return sha1(uniqid((string) mt_rand(), true));
+
+            case 'crypto':
+                if ($len % 2 !== 0) {
+                    throw new InvalidArgumentException(
+                        'You must set an even number to the second parameter when you use `crypto`.'
+                    );
+                }
+
+                return bin2hex(random_bytes($len / 2));
+        }
+        // 'basic' type treated as default
+        return (string) mt_rand();
     }
 }
